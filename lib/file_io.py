8be92410867f6a8411d3e51b626e3f4fdcaa791d
Mutindi Hope <msndindi@outlook.com>
2023-05-26 23:49:15 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', mode="w", encoding="utf-8") as log_file:
+        log_file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', mode="a", encoding="utf-8") as log_file:
+        log_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    log_file = open(f'{file_name}.txt', encoding="utf-8")
+    return log_file.read()
+    
+pass
